c0332a62c81d79da36a4f660c787a325e80e2435
NkamoN <r.nkamo@gmail.com>
2023-10-24 20:11:16 +0200

    testing 0,1,2

--- main.h
+++ main.h
@@ -1,19 +1,20 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#incude <stdlib.h>
 #include <limits.h>
+#include <unistd.h>
 
-#define BUFFER_SIZE 1024
 
 
 /**
-* Structure Format: match conversion specifiers
-* @fmt: format
-* @f: Function associated
-*/
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
 
 typedef struct format
 {
@@ -21,9 +22,28 @@ typedef struct format
 	int (*f)();
 } convert_match;
 
-int _putchar(char c);
-int _printf(const char *format, ...);
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
+int _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif
