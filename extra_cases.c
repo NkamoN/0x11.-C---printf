08be82392329e9cfcecda86aff48b08bf8459cd4
NkamoN <r.nkamo@gmail.com>
2023-10-24 20:51:05 +0200

    Added functions for extra cases

---
+++ extra_cases.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * handle_hex - the function
+ * @num: input
+ * @is_upper: input
+ * @count: count
+ * Return: nothing
+ */
+void handle_hex(unsigned int num, int is_upper, size_t *count)
+{
+print_hex(num, is_upper, count);
+}
+
